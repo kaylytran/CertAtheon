323d6b3849f0738a6aa1254909b23a548d88e2db
Kayly Tran <ktran7895@gmail.com>
2025-02-11 15:20:28 -0500

    separate azure + dotnet .yml

--- obj/Debug/net8.0/CertAtheon.AssemblyInfo.cs
+++ obj/Debug/net8.0/CertAtheon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CertAtheon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb4c14df524424a3d8cca6cb5919caabd8b443f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebf63e3077bac7ec30e5405007e325310d8c9ce6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CertAtheon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CertAtheon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
