84f40dd23e9aeb55d3ad1f9a9e462467c3f51cc4
Kasra Korminejad <kasra76@gmail.com>
2025-02-23 04:14:37 -0500

    feat: add Certificate model

--- Backend/Models/Certificate.cs
+++ Backend/Models/Certificate.cs
@@ -1,1 +1,14 @@
-// Certificate.cs
+using System;
+
+namespace Backend.Models
+{
+    public class Certificate
+    {
+        public int Id { get; set; }
+        public string CertificateName { get; set; }
+        public DateTime CertifiedDate { get; set; }
+        public DateTime ValidTill { get; set; }
+        
+        public string UserId { get; set; }
+    }
+}
