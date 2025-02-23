3a4f1eb458563e64c715e5ec663438cb5e0d4fdb
Kasra Korminejad <kasra76@gmail.com>
2025-02-23 17:53:57 -0500

    fix: add required for all the fields of the Certificate model except UserId
    - make UserId an optional field as the value is filled by the user retrieved using auth token

--- Backend/Models/Certificate.cs
+++ Backend/Models/Certificate.cs
@@ -1,14 +1,22 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Backend.Models
 {
     public class Certificate
     {
         public int Id { get; set; }
+        
+        [Required]
         public string CertificateName { get; set; }
+        
+        [Required]
         public DateTime CertifiedDate { get; set; }
+        
+        [Required]
         public DateTime ValidTill { get; set; }
         
+        // Remove the Required attribute since this will be set server-side.
         public string UserId { get; set; }
     }
 }
