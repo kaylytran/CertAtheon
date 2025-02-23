0a8eec3d2f5ce01d68495069ce3bc93497b42964
Kasra Korminejad <kasra76@gmail.com>
2025-02-23 15:22:45 -0500

    fix: update DBContext with provided IdentityDbContext in Identity package

--- Backend/Data/ApplicationDbContext.cs
+++ Backend/Data/ApplicationDbContext.cs
@@ -1,16 +1,16 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Backend.Models;
 
 namespace Backend.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
-        { }
+        {
+        }
 
         public DbSet<Certificate> Certificates { get; set; }
-        
-        // You can add other DbSets (e.g., for Users) as needed.
     }
 }
