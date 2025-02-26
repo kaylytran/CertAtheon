a2c177b5b8e655baf615af03193f84ec0444606e
Kayly Tran <ktran7895@gmail.com>
2025-02-26 13:46:00 -0500

    frontend

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -81,6 +81,12 @@ if (app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
     StartFrontendServer();
+
+    app.UseStaticFiles(); // Enables serving static files from wwwroot
+    app.UseSpa(spa =>
+    {
+        spa.Options.SourcePath = "wwwroot"; // Serve from wwwroot
+    });
 }
 
 app.UseHttpsRedirection();
