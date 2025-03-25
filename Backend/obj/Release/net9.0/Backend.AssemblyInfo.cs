3d09d6e07eb32b21147c996210eadea322e3091d
Kayly Tran <ktran7895@gmail.com>
2025-03-25 08:00:00 -0400

    changeds in .yml and backend.csproj

--- Backend/obj/Release/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Release/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47da125949ca55bc691af50db49919eeff2c20d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfb9d398ff5a4f7ee0b1696e82cc1e202ab4605d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
