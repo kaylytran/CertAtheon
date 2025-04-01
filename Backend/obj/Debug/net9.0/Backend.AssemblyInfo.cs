7a2954269e8e7d0357e9940c45fd59c312854ba2
Kasra Korminejad <kasra76@gmail.com>
2025-04-01 13:20:14 -0400

    chore: add latest build files

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5beae0b4bc6aa7d3f110861b17cfdb1ae41b78e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ba5e9e83f6cd0b80805e33cae3322ed3b23da29")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
