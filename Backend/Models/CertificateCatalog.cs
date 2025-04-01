84d24f7639f72e687177f8f0040dd03f5aeb93cd
Kasra Korminejad <kasra76@gmail.com>
2025-04-01 13:18:12 -0400

    refactor: minor update

--- Backend/Models/CertificateCatalog.cs
+++ Backend/Models/CertificateCatalog.cs
@@ -6,17 +6,13 @@ namespace Backend.Models
     {
         public int Id { get; set; }
 
-        // Optional description.
-        public string Description { get; set; }
-
         [Required]
         public string CertificateName { get; set; }
 
-        // Category: e.g., Developer, Architect, etc.
         public string Category { get; set; }
-
-        // Optionally, the level of the certificate: e.g., Fundamental, Professional.
+        
         public string CertificateLevel { get; set; }
-
+        
+        public string Description { get; set; }
     }
 }
