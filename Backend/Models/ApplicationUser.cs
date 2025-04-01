02e4327aadb7488eb164197f420b7f5083f144d5
Kasra Korminejad <kasra76@gmail.com>
2025-04-01 13:16:37 -0400

    refactor: rename ProfilePicPath to ProfilePictureUrl

--- Backend/Models/ApplicationUser.cs
+++ Backend/Models/ApplicationUser.cs
@@ -7,7 +7,7 @@ namespace Backend.Models
         // New properties for user details
         public string? FirstName { get; set; }
         public string? LastName { get; set; }        
-        public string? ProfilePicPath { get; set; }
+        public string? ProfilePictureUrl { get; set; }
         public string? JobTitle { get; set; }
         public string? AppRole { get; set; }
         public bool MustChangePassword { get; set; }
