05aa5b8f76091caa3ed465441b0e05ec48f12e7e
Kasra Korminejad <kasra76@gmail.com>
2025-04-21 13:51:07 -0400

    fix: resolve the problem of creating new tables after applying every migrations

---
+++ Backend/Migrations/20250421172650_AddCertificateUrl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCertificateUrl : Migration
+    {
+            /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // instead of creating the entire table, just add your new column:
+            migrationBuilder.AddColumn<string>(
+                name: "DocumentUrl",
+                table: "Certificates",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // on rollback, drop only that column
+            migrationBuilder.DropColumn(
+                name: "DocumentUrl",
+                table: "Certificates");
+        }
+    }
+}
