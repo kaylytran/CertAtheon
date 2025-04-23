4d69a92ead16dc8588c9c984be293f603673b607
Kayly Tran <ktran7895@gmail.com>
2025-04-23 11:30:26 -0400

    updated db

--- Backend/Migrations/20250423152947_InitialCreate.Designer.cs
+++ Backend/Migrations/20250423152947_InitialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250421172650_AddCertificateUrl")]
-    partial class AddCertificateUrl
+    [Migration("20250423152947_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
