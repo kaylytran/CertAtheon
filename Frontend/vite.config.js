e5808829001b748e43c36f4f8b7568f78dd2290d
Kayly Tran <ktran7895@gmail.com>
2025-04-28 14:38:37 -0400

    frontend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     setupFiles: './src/setupTests.js',
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'lcov'],
+      reporter: ['text', 'lcov', 'html'],
       reportsDirectory: './coverage',
     },
     reporters: [
